813e2ed8db8ce35ee4b8be6bba691432f3d539e4
Vojtěch Pospíchal <vojtech.pospichal@zonky.cz>
2018-11-14 19:07:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# convertor
+
+Flutter app for unit converting
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// You can read about packages here: https://flutter.io/using-packages/
+import 'package:convertor/category_route.dart';
+import 'package:flutter/material.dart';
+
+// You can use a relative import, i.e. `import 'category.dart;'` or
+// a package import, as shown below.
+// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
+import 'package:convertor/category.dart';
+
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+/// The function that is called when main.dart is run.
+void main() {
+  runApp(UnitConverterApp());
+}
+
+/// This widget is the root of our application.
+/// Currently, we just show one widget in our app.
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Převod jednotek',
+      home: CategoryRoute(),
+    );
+  }
+}
